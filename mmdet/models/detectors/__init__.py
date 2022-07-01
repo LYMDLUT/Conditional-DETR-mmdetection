840ba2641043a1c50b36269f09653b1cef2d4c96
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2022-07-01 16:01:38 +0800

    1.0

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -3,9 +3,9 @@ from .base import BaseDetector
 from .deformable_detr import DeformableDETR
 from .detr import DETR
 from .two_stage import TwoStageDetector
-
+from .Conditional_detr import ConditionalDETR
 
 
 __all__ = [
-     'BaseDetector', 'DETR',  'DeformableDETR', 'TwoStageDetector'
+     'BaseDetector', 'DETR',  'DeformableDETR', 'TwoStageDetector','ConditionalDETR'
 ]
